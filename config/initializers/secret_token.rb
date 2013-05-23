5964fc6162164dcf496fa1b065ab9acb82a9dcb6
Sameer Rahmani <lxsameer@gnu.org>
2013-05-23 23:49:53 +0430

    Initialized env with basic devise and rails configurations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Darbasti::Application.config.secret_key_base = 'c6213815aaa4fdc083ea7097c8f3a519e212ee86e86c5559b327489a1ab0a9cd16662a598e831e837082098f0ca606b3d60edba3c05314b730d2b72597b6d002'
