c5937045dc2fe0f273ae187a426a63b75d7deb43
Sameer Rahmani <lxsameer@gnu.org>
2013-06-25 20:37:48 +0430

    UI improved, fa locale added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 require "date"
 
 class HomeController < ApplicationController
+  layout "application"
 
   def index
     @advertises = Advertise.where("deactive_date >= ?", DateTime.now).order("cost_per_day DESC").limit(config.first_page_advertises)
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Shakhak
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fa
+
     #config.active_record.whitelist_attributes = false
     ActsAsTaggableOn.force_lowercase = true
     config.ad_unit_size = 100
