58d803d4518db704195cab380a12677c7dc3e4f4
Behnam Ahmad Khan Beigi <b3hnam@gnu.org>
2013-09-30 16:30:11 +0330

    dashboard adv list slide added

---
+++ app/assets/javascripts/dashboard/dashboard.js
@@ -0,0 +1,7 @@
+$(function() {
+    $(".dashboard-show-overload").on("click",function(event){
+        //$(this).preventDefault();
+        $(".overload").not($(this).parent().parent().find(".overload")).slideUp();
+        $(this).parent().parent().find(".overload").slideDown(300);
+    });
+});
